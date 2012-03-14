631dce88887b1285325ccb84ba7b4d3307ebcf61
Steven Raher <SRaher@globys.com>
2012-03-14 11:20:46 -0700

    commit Mar14 am

---
+++ Macro01/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Macro01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ServerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AccountNumber = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BrowserCode = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(843, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // GoButton
+            // 
+            this.GoButton.Location = new System.Drawing.Point(37, 52);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(75, 23);
+            this.GoButton.TabIndex = 1;
+            this.GoButton.Text = "Go";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(37, 81);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputBox.Size = new System.Drawing.Size(301, 290);
+            this.OutputBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Server:";
+            // 
+            // ServerName
+            // 
+            this.ServerName.Location = new System.Drawing.Point(511, 49);
+            this.ServerName.Name = "ServerName";
+            this.ServerName.Size = new System.Drawing.Size(112, 20);
+            this.ServerName.TabIndex = 4;
+            this.ServerName.Text = "QATELAU40";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "User:";
+            // 
+            // UserName
+            // 
+            this.UserName.Location = new System.Drawing.Point(511, 87);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(110, 20);
+            this.UserName.TabIndex = 6;
+            this.UserName.Text = "admin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(444, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password:";
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(511, 125);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(95, 20);
+            this.Password.TabIndex = 8;
+            this.Password.Text = "admin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(444, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Account:";
+            // 
+            // AccountNumber
+            // 
+            this.AccountNumber.Location = new System.Drawing.Point(513, 166);
+            this.AccountNumber.Name = "AccountNumber";
+            this.AccountNumber.Size = new System.Drawing.Size(107, 20);
+            this.AccountNumber.TabIndex = 10;
+            this.AccountNumber.Text = "1525853400";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(443, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Browser:";
+            // 
+            // BrowserCode
+            // 
+            this.BrowserCode.FormattingEnabled = true;
+            this.BrowserCode.Items.AddRange(new object[] {
+            "FX",
+            "V7"});
+            this.BrowserCode.Location = new System.Drawing.Point(513, 204);
+            this.BrowserCode.Name = "BrowserCode";
+            this.BrowserCode.Size = new System.Drawing.Size(120, 17);
+            this.BrowserCode.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 403);
+            this.Controls.Add(this.BrowserCode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.AccountNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.UserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ServerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ServerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AccountNumber;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox BrowserCode;
+    }
+}
+
